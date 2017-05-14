5465d624c3a37c17a7346272048dd9ff0e4e3e9a
Prudhvi Dharmana <prudha@microsoft.com>
2017-05-14 12:25:05 +0530

    comments in model.py

--- model.py
+++ model.py
@@ -14,55 +14,36 @@ imgs = []
 measurements = []
 
 for line in lines:
-    cur_path = line[0]
-    # print(cur_path)
-    # img = cv2.cvtColor(cv2.imread(cur_path), cv2.COLOR_BGR2GRAY)
-    # img = img.reshape(img.shape[0], img.shape[1], 1)
-    if not os.path.exists(cur_path):
-        continue
+    cur_path = line[0] # center image
     img = cv2.imread(cur_path)
     imgs.append(img)
 
-    img = cv2.flip(img, 1)
-    # img = img.reshape(img.shape[0], img.shape[1], 1)
+    img = cv2.flip(img, 1) # horizontal flip
     imgs.append(img)
-    mes = float(line[3])
+
+    mes = float(line[3]) # steering angle for the center image
     measurements.append(mes)
-    measurements.append(-1.0 * mes)
+    measurements.append(-1.0 * mes) # steering angle for flipped image
 
-    # img = cv2.cvtColor(cv2.imread(line[1]), cv2.COLOR_BGR2GRAY)
-    # img = img.reshape(img.shape[0], img.shape[1], 1)
-    imgs.append(cv2.imread(line[1])) # left
-    measurements.append(mes + 0.2)
+    imgs.append(cv2.imread(line[1])) # left camera image
+    measurements.append(mes + 0.2) # correction = +0.2
 
-    # img = cv2.cvtColor(cv2.imread(line[2]), cv2.COLOR_BGR2GRAY)
-    # img = img.reshape(img.shape[0], img.shape[1], 1)
-    imgs.append(cv2.imread(line[2])) # right
-    measurements.append(mes - 0.2)
+    imgs.append(cv2.imread(line[2])) # right camera image
+    measurements.append(mes - 0.2) # correction = -0.2
 
 
 X_train = np.array(imgs)
 y_train = np.array(measurements)
-print(X_train.shape, y_train.shape)
 
 def pre_process(img):
+    # In the preprocessing step, all we do is to simply convert the image from RGB to grayscale
     from keras.backend import tf as ktf
-
-    # sess  = K.get_session()
-    # img = sess.run(img) # now img is a proper numpy array 
-
-    # # img=cv2.cvtColor(img,cv2.COLOR_BGR2YUV) 
-    # # img=cv2.resize(img,(200,66))
-    # # return img
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # img = np.array(img)
-    # return img.reshape((img.shape[0], img.shape[1], 1))
-    print(img)
     return ktf.image.rgb_to_grayscale(img)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Conv2D, Cropping2D
 
+## model which is same as given in nVidia paper
 model = Sequential()
 model.add(Lambda(pre_process, input_shape = (160,320,3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
